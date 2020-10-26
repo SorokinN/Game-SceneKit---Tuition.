1427b77a868a43521233081b2b30d2d826662b15
SorokinN <nikolay@sorokin.dev>
2020-10-26 06:10:10 +0300

    Added difficulty game play.

--- Test-Game-SceneKit/GameViewController.swift
+++ Test-Game-SceneKit/GameViewController.swift
@@ -5,13 +5,13 @@
 //  Created by Nikolay Sorokin on 23.10.2020.
 //
 
-//import UIKit
-//import QuartzCore
 import SceneKit
 
 class GameViewController: UIViewController {
     
     // MARK: - Properties
+    var duration: TimeInterval = 5
+    var score = 0
     var ship: SCNNode!
     
     // MARK: - Methods
@@ -26,9 +26,11 @@ class GameViewController: UIViewController {
         ship.look(at: SCNVector3 (2 * x, 2 * y, 2 * z ))
         
         // Animate ship movement towards camera
-        //        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-        ship.runAction(.move(to: SCNVector3(), duration: 5))
-        
+        ship.runAction(.move(to: SCNVector3(), duration: duration)) {
+        self.ship.removeFromParentNode()
+            
+            print(#line, #function, "Game Over")
+        }
         // retrieve the SCNView
         let scnView = self.view as! SCNView
         
@@ -37,78 +39,68 @@ class GameViewController: UIViewController {
     }
     // TODO: Do something
     func getShip() -> SCNNode {
-        // get scene
+        // Get scene
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
-        // retrieve the ship node
+        // Retrieve the ship node
         let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!.clone()
-        
         return ship
     }
     func removeShip () {
-        // retrieve the SCNView
+        // Retrieve the SCNView
         let scnView = self.view as! SCNView
         
-        // remove the ship
+        // Remove the ship
         scnView.scene?.rootNode.childNode(withName: "ship", recursively: true)?.removeFromParentNode()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // create a new scene
+        // Create a new scene
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
-        // create and add a camera to the scene
+        // Create and add a camera to the scene
         let cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
         scene.rootNode.addChildNode(cameraNode)
         
-        // place the camera
-//        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        // create and add a light to the scene
+        // Create and add a light to the scene
         let lightNode = SCNNode()
         lightNode.light = SCNLight()
         lightNode.light!.type = .omni
         lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
         scene.rootNode.addChildNode(lightNode)
         
-        // create and add an ambient light to the scene
+        // Create and add an ambient light to the scene
         let ambientLightNode = SCNNode()
         ambientLightNode.light = SCNLight()
         ambientLightNode.light!.type = .ambient
         ambientLightNode.light!.color = UIColor.darkGray
         scene.rootNode.addChildNode(ambientLightNode)
         
-        // retrieve the ship node
-//        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
-        
-        // animate the 3d object
-//        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-        
-        // retrieve the SCNView
+        // Retrieve the SCNView
         let scnView = self.view as! SCNView
         
-        // set the scene to the view
+        // Set the scene to the view
         scnView.scene = scene
         
-        // allows the user to manipulate the camera
+        // Allows the user to manipulate the camera
         scnView.allowsCameraControl = true
         
-        // show statistics such as fps and timing information
+        // Show statistics such as fps and timing information
         scnView.showsStatistics = true
         
-        // configure the view
+        // Configure the view
         scnView.backgroundColor = UIColor.black
         
-        // add a tap gesture recognizer
+        // Add a tap gesture recognizer
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         scnView.addGestureRecognizer(tapGesture)
         
-        // remove the chip
+        // Remove the chip
         removeShip()
         
-        // get chip
+        // Get chip
         ship = getShip()
         
         // add ship
@@ -117,32 +109,34 @@ class GameViewController: UIViewController {
     
     @objc
     func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
+        // Retrieve the SCNView
         let scnView = self.view as! SCNView
         
-        // check what nodes are tapped
+        // Check what nodes are tapped
         let p = gestureRecognize.location(in: scnView)
         let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
+        // Check that we clicked on at least one object
         if hitResults.count > 0 {
-            // retrieved the first clicked object
+            // Retrieved the first clicked object
             let result = hitResults[0]
             
-            // get its material
+            // Get its material
             let material = result.node.geometry!.firstMaterial!
             
-            // highlight it
+            // Highlight it
             SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
+            SCNTransaction.animationDuration = 0.2
             
             // on completion - unhighlight
             SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
+                self.ship.removeFromParentNode()
+                self.score += 1
                 
-                material.emission.contents = UIColor.black
+                print(#line, #function, "The ship \(self.score) has been shot")
                 
-                SCNTransaction.commit()
+                self.duration *= 0.95
+                self.ship = self.getShip()
+                self.addShip()
             }
             
             material.emission.contents = UIColor.red
